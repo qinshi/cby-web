373e41a9d0d2a516a5295497fa489400e1f1d1e9
s q <sq@sdeMacBook-Air.local>
2017-05-08 01:04:44 +0800

    inti react

---
+++ app/components/ProductBox.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ProductBox extends Component {
+  render() {
+    return (
+      <div className="productBox">
+        Hello World!
+      </div>
+    );
+  }
+}
---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import ProductBox from './app/components/productBox'
+import 'babel-polyfill'
+const Koa = require('koa');
+const app = new Koa();
+
+// response
+app.use(ctx => {
+    let markup = renderToString(<ProductBox/>);
+    ctx.body = markup;
+});
+
+app.listen(8080);
